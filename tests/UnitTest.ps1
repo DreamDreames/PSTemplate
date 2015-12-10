5f2df8d63858c3886f01a3357ac41862cbbaef42
Rainbowsix <dreamdreames@163.com>
2015-12-10 23:19:17 +0800

    Update test case name and add case of multiple variable

--- tests/UnitTest.ps1
+++ tests/UnitTest.ps1
@@ -1,17 +1,24 @@
 $here = Split-Path $MyInvocation.MyCommand.Path
 
 Describe "Render template With Value"{
-    It "Should get the code to be replaced"{
+    It "Should replace a single value"{
         $temp = '<%= $model.name %>'
         $model = @{name = 'foo'}
         $res = Render $temp $model
         $res | Should Be 'foo'
     } 
 
-    It "Should get code in the middle of template to be replaced"{
+    It "Should replace a single value in a text"{
         $temp = 'hello <%= $model.name %> hello'
         $model = @{name = 'foo'}
         $res = Render $temp $model
         $res | Should Be 'hello foo hello'
+    }
+
+    It "Should replace multiple values in a text"{
+        $temp = 'book name <%= $model.name %>, author <%= $model.author %>'
+        $model = ${name = 'foo'; author = 'bar'}
+        $res = Render $temp $model
+        $res | Should Be 'book name foo, author bar'
     }
 }
