3be77ef6df1a597e29cb89ee60c4ef3ab87ddce0
Rainbowbox <twentyfivehours@163.com>
2015-12-15 23:50:34 +0800

    test

--- go.ps1
+++ go.ps1
@@ -4,7 +4,6 @@ Param(
 
 $root = Split-Path $MyInvocation.MyCommand.Path
 Import-Module "$root\Pester\Pester.psm1"
-#. $root\PsTemplate.ps1
 Import-Module "$root\PsTemplate.psm1" -Force
 
 if( $command -ieq "test"){
