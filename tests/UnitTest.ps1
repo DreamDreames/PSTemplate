3f3478c820e3fb52db2974b6887f743bbee5d37d
Rainbowbox <twentyfivehours@163.com>
2015-12-16 07:13:43 +0800

    Add multiline execute code test

--- tests/UnitTest.ps1
+++ tests/UnitTest.ps1
@@ -1,46 +1,49 @@
 $here = Split-Path $MyInvocation.MyCommand.Path
 $data = "$here\testfiles"
 
-Describe "Render template With Value"{
+Describe "Render Template With Model"{
     It "Should replace a single value"{
         $temp = '<%= $model.name %>'
         $model = @{name = 'foo'}
-        $res = Render $temp $model
-        $res | Should Be 'foo'
+        Render $temp $model | Should Be 'foo'
     } 
 
     It "Should replace a single value in a text"{
         $temp = 'hello <%= $model.name %> hello'
         $model = @{name = 'foo'}
-        $res = Render $temp $model
-        $res | Should Be 'hello foo hello'
+        Render $temp $model | Should Be 'hello foo hello'
     }
 
     It "Should replace multiple values in a text"{
         $temp = 'book name <%= $model.name %>, author <%= $model.author %>'
         $model = @{name = 'foo'; author = 'bar'}
-        $res = Render $temp $model
-        $res | Should Be 'book name foo, author bar'
+        Render $temp $model | Should Be 'book name foo, author bar'
     }
 
     It "Should support nested expression in template"{
         $temp = 'book count <%= $model.Count %> Book Name: <% $model | % { %>book <%= $_ %> <% } %>'
         $model = @("foo", "bar")
-        $res = Render $temp $model
-        $res | Should Be "book count 2 Book Name: book foo book bar "
+        Render $temp $model | Should Be "book count 2 Book Name: book foo book bar "
     }
 
-    It "Should able to render tempalte without model"{
+    It "Should be able to render tempalte without model"{
         $temp = 'this is an empty template'
         Render $temp '' | Should Be $temp
     }
 
-    It "Should able to output newline with evaluate value expression"{
-        $file = "$data\TemplateEvaluateValue.txt"
+    It "Should be able to output newline with evaluate value expression"{
+        $fileName = "TemplateEvaluateValue.txt"
+        $file = "$data\$fileName"
         $model = @{name = 'foo'}
-        $expected = gc "$data\Expected_TemplateEvaluateValue.txt" | Out-String
-        $res = RenderFile $file $model 
-        $res | Out-File C:\temp\test.txt
-        $res | Should Be $expected
+        $expected = gc "$data\Expected_$fileName" | Out-String
+        RenderFile $file $model | Should Be $expected
+    }
+
+    It "Should be able to support nested expression with newline"{
+        $fileName = "TemplateExecuteCode.xml"
+        $file = "$data\$fileName"
+        $model = @{name = 'foo'; author = 'bar'}
+        $expected = gc "$data\Expected_$fileName" | Out-String
+        RenderFile $file $model | Should Be $expected
     }
 }
